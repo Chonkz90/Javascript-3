22c0551531b9d130ef7aa501abd3f52702d960b5
Chonkz90 <zahlerdal@yahoo.com>
2024-08-08 12:02:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javascript-3
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Chonkz90/Javascript-3)
---
+++ script.js
@@ -0,0 +1,62 @@
+/*
+Hente noe fra HTML
+Modifisere det i JS
+Sende det tilbake til HTML
+*/
+
+// const projectCard = document.getElementByID("projectCard");
+
+// En variabel som lager at vi henter et element som har ID-navnet projectCard
+const projectCard = document.querySelector("#projectCard");
+console.log(projectCard);
+
+// en variabel som lagrer at vi lager et nytt element som er h3
+const projectCardHeading = document.createElement("h3");
+console.log (projectCardHeading);
+
+// En variabel som lagrer at vi laget et img element.
+const projectCardImage = document.createElement("img");
+console.log(projectCardImage);
+
+// En variabel som lagrer at vi laget et nytt p element.
+const projectCardInfo = document.createElement("p");
+console.log(projectCardInfo);
+
+// Gir elementet som ble lagret noe text innhold.
+projectCardHeading.textContent = "Mitt prosjekt 2";
+
+// Gir elementet som ble laget en class.
+projectCardHeading.classList.add("projectTitle");
+
+// gir img elementet en src.
+projectCardImage.src = "HousePS1.jpg";
+
+// gir img elementet vi laget en alt
+projectCardImage.alt = "Photoshoppet bilde av Gregory House";
+
+// gir img elementet vi laget en class
+projectCardImage.classlist.add(projectImage);
+
+// Gir p elementet vi laget noe tekst innhold.
+projectCardInfo.textContent= "I dette prosjektet brukte jeg";
+
+// Elementet vi hentet får elementet vi laget
+projectCard.appendChild(projectCardHeading);
+
+// img-elementet vi laget blir lagt til div elementet vi hentet
+projectCard.appendChild(projectCardImage);
+
+// p-elementet vi laget blir lagt til div elementet
+projectCard.appendChild(projectCardInfo);
+
+// 
+// 
+
+const businessCard= document.querySelector("#businessCard");
+
+businessCard.innerHTML = `
+<h3>Tomas Erdal </h3>
+<img class="projectImage"<img src="HousePS1.jpg"
+alt=photoshop />
+<p>Meme Lord</p> `
+
